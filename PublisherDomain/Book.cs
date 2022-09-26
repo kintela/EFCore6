785d742876b1da7a73a749cc3cc4561a7f0b16d8
Kintela <roberto.quintela@gmail.com>
2022-09-26 13:38:42 +0200

    Creacion inicial del proyecto

---
+++ PublisherDomain/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PublisherDomain
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishDate { get; set; }
+        public decimal BasePrice { get; set; }
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
+
