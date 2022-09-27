84bd0dd15dd3a1a40691ca776f7af139645942e2
Kintela <roberto.quintela@gmail.com>
2022-09-27 13:57:17 +0200

    queries

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,15 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.EntityFrameworkCore;
 using PublisherData;
 using PublisherDomain;
 
-using (PubContext context=new PubContext())
+PubContext _context = new PubContext();
+
+FindIt();
+
+
+void FindIt()
 {
-  context.Database.EnsureCreated();
+  var authorIdTwo = _context.Authors.Find(2);
 }
 
-GetAuthors();
-AddAuthor();
-GetAuthors();
+void QueryFilters()
+{
+  var name = "Josie";
+  //var authors = _context.Authors.Where(s => s.FirstName == name).ToList();
+  var filter = "L%";
+  var authors = _context.Authors
+    .Where(a => EF.Functions.Like(a.LastName,filter)).ToList();
+}
 
 void GetAuthors()
 {
@@ -27,6 +38,35 @@ void AddAuthor()
   using var context = new PubContext();
   context.Authors.Add(author);
   context.SaveChanges();
+
+}
+
+void AddAuthorWithBook()
+{
+  var author = new Author { FirstName = "Julie", LastName = "Lerman" };
+  author.Books.Add(new Book { Title = "Programming Entity Framewok", PublishDate = new DateTime(2009, 1, 1) });
+  author.Books.Add(new Book { Title = "Programming Entity Framewok 2nd Ed", PublishDate = new DateTime(2010, 8, 1) });
+
+  using var context = new PubContext();
+  context.Authors.Add(author);
+  context.SaveChanges();
+
+}
+
+void GetAuthorsWithBooks()
+{
+  using var context = new PubContext();
+  var authors = context.Authors.Include(a => a.Books).ToList();
+  foreach (var author in authors)
+  {
+    Console.WriteLine(author.FirstName + " " + author.LastName);
+    foreach (var book in author.Books)
+    {
+      Console.WriteLine("*" + book.Title);
+    }
+  }
+
+  Console.ReadLine();
 
 }
 
