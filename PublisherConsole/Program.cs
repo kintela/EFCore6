b4c661b884506ebf635271b9cf3ed96047258548
Kintela <roberto.quintela@gmail.com>
2022-09-27 10:48:11 +0200

    Añadir un par de autores

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,11 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PublisherData;
+using PublisherDomain;
 
 using (PubContext context=new PubContext())
 {
   context.Database.EnsureCreated();
 }
 
+GetAuthors();
+AddAuthor();
 GetAuthors();
 
 void GetAuthors()
@@ -16,6 +19,15 @@ void GetAuthors()
   {
     Console.WriteLine(author.FirstName + " " + author.LastName);
   }
+}
+
+void AddAuthor()
+{
+  var author = new Author { FirstName = "Josie", LastName = "Newf" };
+  using var context = new PubContext();
+  context.Authors.Add(author);
+  context.SaveChanges();
+
 }
 
 
