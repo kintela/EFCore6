45f62e1d5740c6623a974d6222d8c2c849ce1d84
Kintela <roberto.quintela@gmail.com>
2022-10-11 13:57:37 +0200

    Controllers

---
+++ PubAPI/AuthorDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PubAPI
+{
+  public class AuthorDTO
+  {
+    public int AuthorId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+  }
+}
+
--- PubAPI/Program.cs
+++ PubAPI/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 using PublisherData;
 
@@ -5,7 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+	.AddJsonOptions(opt=>opt.JsonSerializerOptions.ReferenceHandler=ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
